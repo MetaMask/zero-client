4725c4005b7eed08187618712a1b2b547c7ad00f
kumavis <aaron@kumavis.me>
2016-02-09 15:58:58 -0800

    use default zero-client from provider-engine

--- frame.js
+++ frame.js
@@ -1,13 +1,20 @@
-const ZeroClientProvider = require('./zero-provider.js')
+const ZeroClientProvider = require('web3-provider-engine/zero')
 const ParentStream = require('iframe-stream').ParentStream
 const StreamHandler = require('web3-stream-provider/handler.js')
 
 console.log('yes, this is iframe')
-initializeZeroClient()
+
+initializeZeroClient({
+  rpcUrl: 'https://rpc.metamask.io/',
+})
 
 
 function initializeZeroClient(opts) {
-  var zeroClientProvider = new ZeroClientProvider()
+  var zeroClientProvider = new ZeroClientProvider(opts)
+  zeroClientProvider.on('block', function(block){
+    console.log('BLOCK CHANGED:', '#'+block.number.toString('hex'), '0x'+block.hash.toString('hex'))
+  })
+
   var iframeStream = new ParentStream()
   var handlerStream = new StreamHandler(zeroClientProvider)
   
